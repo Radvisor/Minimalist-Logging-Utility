52d7b5b7f733efa56838a05676e5229cee76917f
Radvisor <166956667+Radvisor@users.noreply.github.com>
2025-04-21 04:52:37 +0200

    Add files via upload

---
+++ exemple.cpp
@@ -0,0 +1,22 @@
+#include <windows.h>
+
+#include "debug.h"
+
+using namespace std;
+
+int main(void)
+{
+	InitDebugLogs(L"C:\\Users\\exemple\\Desktop\\logs.txt");
+
+	//Lets' try to retrieve the handle of a DLL that shouldn't be present
+	DebugComment(L"Attempting to call GetModuleHandle :");
+
+	HMODULE hDll = GetModuleHandleW(L"Exemple.dll");
+	if(hDll)
+	{
+		DebugComment(L"Success!");
+	}
+	else DebugErrorCode( GetLastError() );
+
+	return 0;
+}
