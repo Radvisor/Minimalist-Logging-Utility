867a71b8f189bfc4e0250d1c4c09a8dbcd0b6f98
Radvisor <166956667+Radvisor@users.noreply.github.com>
2025-04-21 04:18:15 +0200

    Add files via upload

---
+++ debug.cpp
@@ -0,0 +1,93 @@
+#include <windows.h>
+
+#include <sal.h>
+
+#include "debug.h"
+
+DebugLogs::DebugLogs( _In_ LPCWSTR pwszLogFilePath )
+{
+	////////////////////////////////////////////////////////////////////////////////////////////////////
+	
+	////////////////////////////////////////////////////////////////////////////////////////////////////
+	
+	this->hLogFile = CreateFileW( 	pwszLogFilePath, GENERIC_WRITE, FILE_SHARE_READ | FILE_SHARE_WRITE,
+									NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL );
+}
+
+DebugLogs::~DebugLogs()
+{
+	////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////
+	
+	CloseHandle(this->hLogFile);
+}
+
+_Success_(return == TRUE )
+BOOL DebugLogs::AddString( _In_ LPCWSTR pwszLogString, _In_ DWORD dwStringSize )
+{
+	////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	BOOL bReturnValue = FALSE;
+
+	DWORD dwBytesWritten = 0;
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////
+	
+	if( !WriteFile( this->hLogFile, pwszLogString, dwStringSize, &dwBytesWritten, NULL ) )
+		goto end;
+	if( dwBytesWritten != dwStringSize )
+		goto end;
+	
+	bReturnValue = TRUE;
+
+end:
+	return bReturnValue;
+}
+
+_Success_(return == TRUE )
+BOOL DebugLogs::AddErrorCode( _In_ DWORD dwErrorCode )
+{
+	////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	BOOL bReturnValue = FALSE;
+	
+	//First part's length + number of decimals to show + escape character + string terminator.
+	WCHAR wszErrorStatement[14 + 10 + 1 + 1] = L"Error code: 0d";
+	WCHAR wszAdditionalStatement[] = L"The error code might not be relevant if it's a pointer error for exemple.\n";
+	
+	////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	this->ConvertDwordToString( dwErrorCode, &wszErrorStatement[14] );	//Address of the decimal part.
+	
+	if( !this->AddString( wszErrorStatement , sizeof(wszErrorStatement) ) )
+		goto end;
+	
+	if( !this->AddString( wszAdditionalStatement, sizeof(wszAdditionalStatement) ) )
+		goto end;
+	
+	bReturnValue = TRUE;
+
+end:
+	return bReturnValue;
+}
+
+void DebugLogs::ConvertDwordToString( _In_ DWORD dwErrorCode, _Out_ LPWSTR pwszErrorCode )
+{
+	////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    DWORD dwDivisor = 1000000000;
+	DWORD dwDigit;
+	
+	////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    for( auto i = 0; i < 10; i++ )
+    {
+        dwDigit = dwErrorCode / dwDivisor;
+        pwszErrorCode[i] = (WCHAR)(L'0' + dwDigit);
+        dwErrorCode %= dwDivisor;
+        dwDivisor /= 10;
+    }
+	pwszErrorCode[10] = L'\n';
+	pwszErrorCode[10] = L'\0';
+}
---
+++ debug.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <windows.h>
+#include <winternl.h>
+
+#include <sal.h>
+
+#define InitDebugLogs(pwszLogFilePath) \
+		DebugLogs debugLogs(pwszLogFilePath)
+
+#define DebugComment(pwszLogString) \
+		debugLogs.AddString( pwszLogString L"\n", sizeof(pwszLogString) + sizeof(L'\n') )
+
+#define DebugErrorCode(dwErrorCode) \
+		debugLogs.AddErrorCode(dwErrorCode)
+
+class DebugLogs
+{
+	////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	private:
+		HANDLE hLogFile;
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	public:
+		DebugLogs( _In_ LPCWSTR pwszLogFilePath );
+		~DebugLogs();
+		
+		_Success_(return == TRUE ) BOOL AddString( _In_ LPCWSTR pwszLogString, _In_ DWORD dwStringSize );
+		_Success_(return == TRUE ) BOOL AddErrorCode( _In_ DWORD dwErrorCode );
+	
+	private:
+		void ConvertDwordToString( _In_ DWORD dwErrorCode, _Out_ LPWSTR pwszErrorCode );
+};
